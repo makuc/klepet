1532c6fb83541d2e5a2880efe79cac6997a845b1
Armin Makovec <armin.makovec@hotmail.com>
2016-04-08 17:03:52 +0000

    Odstranjena pomožna funkcija pokaziYotube

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -139,10 +139,6 @@ function dodajSmeske(vhodnoBesedilo) {
   }
   return vhodnoBesedilo;
 }
-function pokaziYoutube(vhodnoBesedilo)
-{
-  return "";
-}
 function pokaziSlike(vhodnoBesedilo) {
   var i = vhodnoBesedilo.indexOf("http://");
   var is = vhodnoBesedilo.indexOf("https://");
